a4501d7da056f933b0ead494f17dac1c2816b831
Omer Ali Malik <omeralimalik96@gmail.com>
2024-07-11 19:57:54 +0500

    connected /settings/profile to backend and displayed data with editable fields component

--- app/(tabs)/settings/profile.tsx
+++ app/(tabs)/settings/profile.tsx
@@ -11,11 +11,70 @@ import { AntDesign } from '@expo/vector-icons';
 
 import {Back} from "../_common"
 
-export default function ProfilePage(){
+import * as api from "../api"
+
+interface EditableProps {
+  name : string;
+  value : string;
+  onChange   : Function;
+}
+function EditableField(props : EditableProps){
   return (
-    <View style={{backgroundColor : "#121212"}}>
-      <Back text="Settings"/>
-      <Text style={{fontSize : 30 , color : "white"}}>Hello World</Text>
+    <View style={{
+      display : "flex" , 
+      flexDirection : "row" , 
+      justifyContent : "space-between" , 
+      marginHorizontal : 20,
+      marginVertical : 20,
+    }}>
+      <Text style={{
+          fontSize : 23, 
+          color : "white", 
+          alignSelf : "center", 
+          fontFamily : "Montserrat", 
+        }}>
+        {props.name}
+      </Text>
+      <Text style={{
+          fontSize : 20, 
+          color : "white", 
+          alignSelf : "center", 
+          fontFamily : "Montserrat", 
+          fontWeight :"bold"
+        }}>
+        {props.value.length > 10 ? props.value.substring(0,9) + " ..." : props.value}
+      </Text>
     </View>
   )
+}
+
+export default function ProfilePage(){
+    const [data , setData] = useState({
+        id : "",
+        avatar: "",
+        name: "",
+        number: "",
+        username: "",
+        email: "",
+        password: ""
+    })
+    useEffect(() => {
+        (async () => {
+        const resp = await api.getDetails();
+        setData(resp);
+        })()
+    } , [])
+    return (
+        <ScrollView style={{ flex: 1, backgroundColor: '#121212' }}>
+          <Back text="Settings"/>
+          <View style={{marginTop : 50}}></View>
+
+          <EditableField name="Username" value={data.username} onChange={() => {}}/>
+            
+          <EditableField name="Email" value={data.email} onChange={() => {}}/>
+
+          <EditableField name="Phone" value={"+92 300 0856955"} onChange={() => {}}/>
+
+        </ScrollView>
+    )
 }
