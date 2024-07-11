eb9c729eea22fac229b2c7c3aee78aeec309c76d
Omer Ali Malik <omeralimalik96@gmail.com>
2024-07-11 21:51:31 +0500

    completed /settings/about page

--- app/(tabs)/settings/about.tsx
+++ app/(tabs)/settings/about.tsx
@@ -1,21 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, ImageBackground, FlatList, Image, View, Text, ScrollView, Pressable, Button} from 'react-native';
-import * as Font from "expo-font";
-import {router} from "expo-router";
-import { moderateScale, scale, verticalScale } from 'react-native-size-matters';
-import * as size from "react-native-size-matters";
-import { BACKGROUND_COLOR } from '@birdwingo/react-native-instagram-stories/src/core/constants';
-import { EvilIcons } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
-
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  ScrollView,
+  Text,
+  Pressable,
+  Switch,
+  Image,
+} from 'react-native';
+import {router} from "expo-router"
+import { 
+  FontAwesome, FontAwesome6, MaterialIcons, Feather,
+  Ionicons,
+} from '@expo/vector-icons';
+import * as size from "react-native-size-matters"
 import {Back} from "../_common"
 
-export default function ProfilePage(){
+import * as api from "../api"
+
+const aboutText = "Juno is dedicated to making online shopping easier and more personalized.\n\n\nOur platform allows users to connect with brands, receive tailored recommendations, and discover new products effortlessly.\n\n\nWith a focus on privacy and security, Juno is a trusted space for women to sell homemade products and small businesses to thrive.\n\n\nJoin Juno today and experience the future of e-commerce!"
+
+export default function SettingsPage() {
+  // TODO : Get User details from backend
   return (
-    <View style={{backgroundColor : "#121212"}}>
+    <ScrollView style={{ flex: 1, backgroundColor: '#121212', paddingBottom : 100,}}>
       <Back text="Settings"/>
-      <Text style={{fontSize : 30 , color : "white"}}>Hello World</Text>
-    </View>
-  )
+      <Text style={{
+          marginHorizontal : 20,
+        color : "white", 
+        fontSize : 24,
+        fontFamily : "Poppins",
+        fontWeight : "bold",
+        marginVertical : 40,
+        }}>
+        About Juno
+        </Text>
+      <Text style={{marginHorizontal : 20, color : "white", fontSize : 20,fontFamily : "Poppins"}}>
+        {aboutText}
+      </Text>
+    </ScrollView>
+  );
 }
--- app/(tabs)/settings/help.tsx
+++ app/(tabs)/settings/help.tsx
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, ImageBackground, FlatList, Image, View, Text, ScrollView, Pressable, Button} from 'react-native';
-import * as Font from "expo-font";
-import {router} from "expo-router";
-import { moderateScale, scale, verticalScale } from 'react-native-size-matters';
-import * as size from "react-native-size-matters";
-import { BACKGROUND_COLOR } from '@birdwingo/react-native-instagram-stories/src/core/constants';
-import { EvilIcons } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
-
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  ScrollView,
+  Text,
+  Pressable,
+  Switch,
+  Image,
+} from 'react-native';
+import {router} from "expo-router"
+import { 
+  FontAwesome, FontAwesome6, MaterialIcons, Feather,
+  Ionicons,
+} from '@expo/vector-icons';
+import * as size from "react-native-size-matters"
 import {Back} from "../_common"
 
-export default function ProfilePage(){
+import * as api from "../api"
+
+export default function SettingsPage() {
+  // TODO : Get User details from backend
   return (
-    <View style={{backgroundColor : "#121212"}}>
+    <ScrollView style={{ flex: 1, backgroundColor: '#121212' }}>
       <Back text="Settings"/>
-      <Text style={{fontSize : 30 , color : "white"}}>Hello World</Text>
-    </View>
-  )
+      <View style={{marginTop : 100}}></View>
+
+    </ScrollView>
+  );
 }
